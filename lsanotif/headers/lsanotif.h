70d4c657a2549517309da9864e811bd301273305
d3adzo <enzo.destephano@gmail.com>
2022-02-03 13:56:02 -0500

    fixed lsa header file

--- lsanotif/headers/lsanotif.h
+++ lsanotif/headers/lsanotif.h
@@ -1,14 +1,15 @@
 #pragma once
 
 #include "pch.h"
-#include "stdafx.h"
 #include <WinSock2.h>
 #include <Windows.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <NTSecAPI.h>
 
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
 
 #define rKey L"SOFTWARE\\kindtime"
 
-void DWORD getPtReg();
+DWORD getPtReg();
 char* getIPReg(const wchar_t*);
